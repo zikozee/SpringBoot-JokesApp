9ade932d39f70a62fb8d4f32e0ae5f0600cc10b5
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2019-09-19 11:22:21 +0100

    Using xml config

--- src/main/java/com/zikozee/springboot/jokesapp/JokesappApplication.java
+++ src/main/java/com/zikozee/springboot/jokesapp/JokesappApplication.java
@@ -2,8 +2,10 @@ package com.zikozee.springboot.jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokesappApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/zikozee/springboot/jokesapp/config/ChuckConfiguration.java
+++ src/main/java/com/zikozee/springboot/jokesapp/config/ChuckConfiguration.java
@@ -1,16 +1,14 @@
 package com.zikozee.springboot.jokesapp.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Using Java Configuration
  * We are bringing in here and external dependency
  */
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
-    @Bean
+//    @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
